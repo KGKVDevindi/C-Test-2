178330b983dc5eea6ccf5f1456005b6806987fa8
ADMIN <ADMIN@VISH>
2024-01-28 22:59:42 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }    }
+}
